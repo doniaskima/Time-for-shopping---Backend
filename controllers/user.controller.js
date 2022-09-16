6c443e28f9d3f2c534dc9e8715638bab41aafcd8
doniaskima <doniaskima344@gmail.com>
2022-09-16 13:15:15 +0100

    user.controller done

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,9 +1,7 @@
-const _ = require('lodash');
+const _ = require("lodash");
 const User = require("../models/user.model");
 const bcrypt = require("bcrypt");
-const jwt = require("jesonwebtoken");
-
-
+const jwt = require("jsonwebtoken");
 const loginUserAndSendCredentials = async(req, res) => {
     try {
         const { email, password } = req.body;
@@ -32,3 +30,34 @@ const loginUserAndSendCredentials = async(req, res) => {
         return res.json({ status: false, message: error.message });
     }
 };
+
+const signupUserAndSendCredentials = async(req, res) => {
+    try {
+        const { user } = req.body;
+        const isAlreadyExist = await User.findOne({ email: user.email }).catch(
+            (err) => console.log(err)
+        );
+        if (isAlreadyExist) {
+            return res.json({
+                status: false,
+                message: "Email already exist, try login instead",
+            });
+        }
+        const salt = await bcrypt.genSalt(10);
+        user.password = await bcrypt.hash(user.password, salt);
+        let newUser = new User(user);
+        newUser = await newUser.save();
+        const token = jwt.sign({ _id: user._id }, process.env.JWT_SECRET, {
+            expiresIn: "24h",
+        });
+        return res.json({
+            status: true,
+            user: _.pick(newUser, ["_id", "name", "email"]),
+            token: token,
+        });
+    } catch (error) {
+        return res.json({ status: false, message: error.message });
+    }
+};
+
+module.exports = { loginUserAndSendCredentials, signupUserAndSendCredentials };
