a79044cab3b5c832698fd2303b455717f4ae364f
doniaskima <doniaskima344@gmail.com>
2022-09-17 01:24:07 +0100

    addToWishlist: added and tested :)

--- controllers/wishlist.controller.js
+++ controllers/wishlist.controller.js
@@ -31,4 +31,42 @@ const fetchWishlistItems = async(req, res) => {
     }
 }
 
-module.exports = { fetchWishlistItems };
+const addToWishlist = async(req, res) => {
+    try {
+        const { userId, productId } = req.params;
+        const user = await User.findById(userId);
+        const wishlist = await Wishlist.findById(user.wishlist);
+        if (wishlist) {
+            const alreadyExist = wishlist.items.some(
+                (product) => product.toString() == productId
+            );
+            if (!alreadyExist) {
+                wishlist.items.push(productId);
+                await wishlist.save();
+            }
+        } else {
+            let newWishlist = new Wishlist({
+                items: [productId],
+                user: userId,
+            });
+            newWishlist = await newWishlist.save();
+            await user.updateOne({ wishlist: newWishlist._id });
+        }
+        const newUser = await User.findById(userId);
+        const wishlistItems = await Wishlist.findById(newUser.wishlist).populate(
+            "items"
+        );
+        return res.json({
+            success: true,
+            items: wishlistItems,
+            message: "product added to wishlist",
+        });
+    } catch (error) {
+        return res.json({
+            success: false,
+            message: error.message,
+        });
+    }
+}
+
+module.exports = { fetchWishlistItems, addToWishlist };
--- routers/wishlist.router.js
+++ routers/wishlist.router.js
@@ -1,8 +1,11 @@
 const { Router } = require("express");
 const router = Router();
-const { fetchWishlistItems } = require("../controllers/wishlist.controller");
-
+const {
+    fetchWishlistItems,
+    addToWishlist,
+} = require("../controllers/wishlist.controller");
 
 router.route("/fetch-wishlist/:userId").get(fetchWishlistItems);
+router.route("/add-item/:userId/:productId").post(addToWishlist);
 
 module.exports = router;
